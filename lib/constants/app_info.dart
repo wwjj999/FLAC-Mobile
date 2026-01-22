91ffb2502727c451a4d2c79bc30f73bc453cfd75
zarzet <zarzet@users.noreply.github.com>
2026-01-22 07:06:15 +0700

    chore: bump version to 26.2.1+65 (new year.month.day format)

--- lib/constants/app_info.dart
+++ lib/constants/app_info.dart
@@ -1,8 +1,8 @@
 /// App version and info constants
 /// Update version here only - all other files will reference this
 class AppInfo {
-static const String version = '3.2.1';
-  static const String buildNumber = '64';
+static const String version = '26.2.1';
+  static const String buildNumber = '65';
   static const String fullVersion = '$version+$buildNumber';
   
   
